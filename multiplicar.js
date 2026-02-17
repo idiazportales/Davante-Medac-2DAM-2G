b4323a7dcc5f19ae42277f00a26ed3a853c98460
isaac <isaacalonsojrz@gmail.com>
2026-02-17 15:27:32 +0100

    fusion en la main

--- multiplicar.js
+++ multiplicar.js
@@ -1,18 +1,4 @@
-// 1. Capturamos el botón usando el ID exacto del HTML ("multiplicar")
-let botonMultiplicar = document.getElementById("multiplicar");
-
-// 2. Escuchamos el evento click
-botonMultiplicar.addEventListener("click", multiplicar);
-
-// 3. La función que realiza la lógica
-function multiplicar() {
-    // Capturamos los valores de los inputs JUSTO cuando se hace clic
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-
-    // Realizamos la operación usando parseFloat para permitir decimales
-    let resultado = parseFloat(num1) * parseFloat(num2);
-
-    // Mostramos el resultado en el párrafo con id="resultado"
-    document.getElementById("resultado").innerHTML = "El resultado de la multiplicación es: " + resultado;
+// Exportamos la función restar
+export function multiplicar(a, b) {
+    return a * b;
 }
